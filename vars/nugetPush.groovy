4cefe15011229116dfc5a2838349263ebe41cbd3
denisgaina <denis.gaina@srco.utm.md>
2020-10-29 02:35:17 +0200

    Update nugetPush.groovy

--- vars/nugetPush.groovy
+++ vars/nugetPush.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {   
     try {
-        def status = bat returnStatus: true, script: "${config.nugetPath} push ${config.packagePath} ${config.nexusKey} -source ${config.source} && exit %%ERRORLEVEL%% "
+        def status = bat returnStatus: true, script: "${config.nugetPath} push ${config.packagePath} ${config.nexusKey} -source ${config.source}"
         if ( status != 0 ){
             error("Nuget push of ${config.packagePath} failed")
         }
