c9357ec8e90bf97724cb0b57171056620e249b9f
Denis Gaina <denis.gaina@srco.utm.md>
2020-10-29 02:11:06 +0200

    first commit

---
+++ vars/nugetPush.groovy
@@ -0,0 +1,10 @@
+def call(Map config) {   
+    try {
+        def status = bat returnStatus: true, script: "${config.nugetPath} push ${config.packagePath} ${config.nexusKey} -source ${config.source} && exit %%ERRORLEVEL%% "
+        if ( status != 0 ){
+            error("Nuget push of ${config.packagePath} failed")
+        }
+    } catch (err) {
+        error("Nuget push of ${config.packagePath} failed ${err}")
+    }
+}
