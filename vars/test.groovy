eb37f3bc62d73b3e23d59c5ed637427eea9069cd
denisgaina <gainadenis7@gmail.com>
2021-04-28 14:12:46 +0300

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,17 @@
+def call(Closure body) {
+    int a = 2
+    if (a<100) { 
+        //If the condition is true print the following statement 
+        println("The value is less than 100");
+        body()
+    }
+    else {
+                
+ println("random text");
+                body()
+ 
+                      
+            
+        
+    }
+}
